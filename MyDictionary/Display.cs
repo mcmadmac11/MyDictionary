be75305793c45ea32ccfb845b98e8a42a05da5df
a <a>
2016-01-06 10:54:15 -0600

    added display class/functionality

---
+++ MyDictionary/Display.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+     public class Display
+    {
+
+        List <string> hashedUserName = new List<string>();
+
+         public string DefUserName()
+        {
+            Console.WriteLine("Enter desired user name: ");
+            string userNameIsSet = Console.ReadLine();
+
+            return userNameIsSet;
+            
+        }
+
+
+    }
+}
--- MyDictionary/Program.cs
+++ MyDictionary/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyDictionary;
 
 namespace MyDictionary
 {
@@ -10,13 +11,10 @@ namespace MyDictionary
     {
         static void Main(string[] args)
         {
-            // Two-dimensional array.
-            int[,] array2D = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
-            // The same array with dimensions specified.
-            int[,] array2Da = new int[4, 2] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
-            // A similar array with string elements.
-            string[,] array2Db = new string[3, 2] { { "one", "two" }, { "three", "four" },
-                                        { "five", "six" } };
+            Display disp = new Display();
+            
+            Console.WriteLine("Your User Name is: {0}", disp.DefUserName());
         }
+
     }
 }
