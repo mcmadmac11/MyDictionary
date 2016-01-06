6130b3f8f384cea09c472795e4d3500e43b4e26e
Dan McManus <mcmanusdbm@gmail.com>
2016-01-05 18:11:38 -0600

    added notes.txt file

--- MyDictionary/Program.cs
+++ MyDictionary/Program.cs
@@ -10,6 +10,13 @@ namespace MyDictionary
     {
         static void Main(string[] args)
         {
+            // Two-dimensional array.
+            int[,] array2D = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
+            // The same array with dimensions specified.
+            int[,] array2Da = new int[4, 2] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
+            // A similar array with string elements.
+            string[,] array2Db = new string[3, 2] { { "one", "two" }, { "three", "four" },
+                                        { "five", "six" } };
         }
     }
 }
